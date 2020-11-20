638fdb404eb3785ad9e5513cd8e1548122b0466e
Salv-Dev <salv.dev.br@gmail.com>
2020-11-20 20:05:49 -0300

    putUser function

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -17,3 +17,31 @@ exports.postUser = (req, res, next) => {
         "success": "201 - created"
     });
 }
+
+exports.putUser = (req, res, next) => {
+    if(isNaN(req.params.id)){
+        res.status(400).json({
+            "error": "400 - Bad Request"
+        });
+    }
+
+    const user = tb_users.find(user => user.id == req.params.id);
+
+    if(user != undefined){
+        const { name, email, pass, avatar_url } = req.body;
+        name != undefined ? user.name = name : null;
+        email != undefined ? user.email = email : null;
+        pass != undefined ? user.pass = pass : null;
+        avatar_url != undefined ? user.avatar_url = avatar_url : null;
+    
+        res.status(200).json({
+            "success": "200 - Success",
+            "users": tb_users
+        });
+    } else {
+        res.status(404).json({
+            "error": "404 - Not Found"
+        });
+    }
+
+}
