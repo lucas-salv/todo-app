909daf45caabd62f195315260f7cb35d29d3a6e5
Salv-Dev <47800892+Salv-Dev@users.noreply.github.com>
2020-11-26 00:16:58 -0300

    routes.js created

--- server/index.js
+++ server/index.js
@@ -3,16 +3,20 @@ const app = express();
 const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const routes = require('./routes/routes');
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/api/v1', routes);
+
 app.get('/', (req, res) => {
     res.status(200).json({
-        'api': 'hello'
-    })
+        'message': 'todo list api - version:1.0.0'
+    });
 });
+
 
 server.listen(4000, () => console.log('API is running!'));
