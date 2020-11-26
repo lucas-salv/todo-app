e7e3396e678e329e4e8cbf7e882fb88151c9fa5b
Salv-Dev <47800892+Salv-Dev@users.noreply.github.com>
2020-11-26 01:24:15 -0300

    static files completed

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/todo-app/static/', express.static('public'));
+
 app.use('/api/v1', routes);
 
 app.get('/', (req, res) => {
