d008e779954f06694d50ad782a9239cd3d7a6e15
Salv-Dev <salv.dev.br@gmail.com>
2020-11-28 20:44:27 -0300

    some corrections made to these files

--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes/routes');
+const { tb_users } = require('./models/database');
+const jwt = require('./utils/jwt');
 
+// configs
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// static files
 app.use('/todo-app/static/', express.static('public'));
 
+// routes
 app.use('/api/v1', routes);
 
 app.get('/api/v1', (req, res) => {
@@ -20,6 +24,30 @@ app.get('/api/v1', (req, res) => {
         'message': 'todo list api - version:1.0.0'
     });
 });
+
+app.get('/api/v1/login', (req, res) => {
+    const [, hash] = req.headers.authorization.split(' ');
+    const [email, pass] = Buffer.from(hash, 'base64').toString().split(':');
+
+    const user = tb_users.find(user => user.email == email && user.pass == pass);
+
+    delete user.pass;
+
+    if(!user) {
+        return res.status(404).json({
+            "message": "404 - Not Found"
+        });
+    }
+
+    const token = jwt.sign({ user: user.id })
+
+    res.status(200).json({
+        "message": "200 - Success",
+        user,
+        token
+    });
+
+})
 
 
 server.listen(process.env.SERVER_PORT, () => console.log('API is running!'));
--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,4 +1,4 @@
 const jwt = require('jsonwebtoken');
 
 exports.sign = payload => jwt.sign(payload, process.env.SECRET, { expiresIn: 86400 });
-exports.verify = token => jwt.verify(token, process.env.SECRET);
+exports.verify = (token, callback) => jwt.verify(token, process.env.SECRET, callback);
