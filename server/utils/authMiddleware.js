97400cf0c7a4c82feeca2ef78095f176284e2504
Salv-Dev <salv.dev.br@gmail.com>
2020-11-28 21:21:37 -0300

    auth in routes

--- server/utils/authMiddleware.js
+++ server/utils/authMiddleware.js
@@ -2,21 +2,28 @@ const jwt = require('./jwt');
 const { tb_users } = require('./../models/database');
 
 module.exports = (req, res, next) => {
-    const [, token] = req.headers.authorization.split(' ');
+    try {
+        const [, token] = req.headers.authorization.split(' ');
+    
+        jwt.verify(token, function(err, decoded){
+            if(err) {
+                return res.status(401).json({
+                    "message": "401 - unauthorized"
+                });
+            }
+    
+            const user = tb_users.find(user => user.id == decoded.user);
+    
+            delete user.pass;
+    
+            req.auth = user;
+    
+            next();
+        })
 
-    jwt.verify(token, function(err, decoded){
-        if(err) {
-            return res.status(401).json({
-                "message": "401 - unauthorized"
-            });
-        }
-
-        const user = tb_users.find(user => user.id == decoded.user);
-
-        delete user.pass;
-
-        req.auth = user;
-
-        next();
-    })
+    } catch(err) {
+        return res.status(401).json({
+            "message": "401 - unauthorized"
+        })
+    }
 }
