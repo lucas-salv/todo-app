2c83af19698ec62d07ac7b5e8691a90ced476d42
Salv-Dev <salv.dev.br@gmail.com>
2020-12-06 01:48:30 -0300

    add Logo stories

---
+++ web/src/stories/Logo.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './../components/Logo';
+
+const defaultObj = {
+    title: 'components/Logo',
+    component: Logo
+};
+
+export default defaultObj;
+
+export const Default = () => <Logo />
