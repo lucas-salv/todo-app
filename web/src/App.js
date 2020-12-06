218f09cdab8603a45f159b644387173418be43fb
Salv-Dev <salv.dev.br@gmail.com>
2020-12-06 01:37:06 -0300

    add button story and config themeProvider and globalStyles in storybook

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,9 @@
 import Login from './pages/Login';
 import { colors } from './utils/colors';
 
-// global styles
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import GlobalStyles from './utils/globalStyles';
+import { ThemeProvider } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    letter-spacing: 1px;
-  }
-`
 
 export default function App() {
   return (
---
+++ web/src/stories/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './../components/Button';
+
+const componentObj = {
+    title: "components/Button",
+    component: Button
+}
+
+export default componentObj;
+
+export const Default = () => <Button>Default</Button>
