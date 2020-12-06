743fac927a315417d5ef7971eefaff8abe5c5753
Salv-Dev <salv.dev.br@gmail.com>
2020-12-06 00:26:46 -0300

    add title in formLogin component

--- web/src/components/FormLogin/index.js
+++ web/src/components/FormLogin/index.js
@@ -2,12 +2,13 @@ import { FiUser, FiLock } from 'react-icons/fi'
 import Button from './../Button';
 import Link from './../Link';
 import Logo from './../Logo';
-import { Form, Label } from './styles';
+import { Form, Label, Title } from './styles';
 
 export default function FormLogin() {
     return (
         <Form action="#">
             <Logo />
+            <Title>Faça seu login</Title>
             <Label htmlFor="email">
                 <FiUser color="#9B9B9B" />
                 <input type="text" id="email" name="email" autoFocus />
