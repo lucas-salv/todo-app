16b77e67658d909bd167eda9afb8e124396a998f
Salv-Dev <salv.dev.br@gmail.com>
2020-12-07 19:20:33 -0300

    CreateAccount stories created

---
+++ web/src/stories/CreateAccountPage.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CreateAccount from './../pages/CreateAccount';
+
+const defaultObj = {
+    title: 'pages/CreateAccount',
+    component: CreateAccount
+};
+
+export default defaultObj;
+
+export const Default = () => <CreateAccount />
