43b417048d025464dda205f5361b1a4af6cf4da3
Salv-Dev <salv.dev.br@gmail.com>
2020-12-07 18:22:28 -0300

    add media queries in FormCreateAccount component

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import FormLogin from './../../components/FormLogin';
+import FormLogin from './../../components/FormCreateAccount';
 import { Container } from './styles';
 
 export default function Login(){
