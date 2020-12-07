18bb12d436d388609a5458fd40b194e2431e9700
Salv-Dev <47800892+Salv-Dev@users.noreply.github.com>
2020-12-07 18:45:10 -0300

    add FormLogin component stories

---
+++ web/src/stories/FormLogin.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormLogin from './../components/FormLogin';
+
+const defaultObj = {
+    title: 'components/FormLogin',
+    component: FormLogin
+};
+
+export default defaultObj;
+
+export const Responsive = () => <FormLogin />
