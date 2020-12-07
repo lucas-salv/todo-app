02787b2a0e19f93c02cfa14371a7e88362125aee
Salv-Dev <salv.dev.br@gmail.com>
2020-12-07 19:28:09 -0300

    Login stories added

---
+++ web/src/stories/LoginPage.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './../pages/Login';
+
+const defaultObj = {
+    title: 'pages/Login',
+    component: Login
+};
+
+export default defaultObj;
+
+export const Default = () => <Login />
