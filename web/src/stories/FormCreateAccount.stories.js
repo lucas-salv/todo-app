de28cd9584794a1df80b61117f42dd516e08df8f
Salv-Dev <salv.dev.br@gmail.com>
2020-12-07 19:08:26 -0300

    add FormCreateAccount stories

---
+++ web/src/stories/FormCreateAccount.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormCreateAccount from './../components/FormCreateAccount';
+
+const defaultObj = {
+    title: 'components/FormCreateAccount',
+    component: FormCreateAccount
+};
+
+export default defaultObj;
+
+export const Default = () => <FormCreateAccount />
