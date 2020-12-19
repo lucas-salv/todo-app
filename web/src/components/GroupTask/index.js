712ebd56ae40d816d8a4195fa8087db7bbbb0172
Salv-Dev <salv.dev.br@gmail.com>
2020-12-18 23:28:44 -0300

    addGroupForm added

--- web/src/components/GroupTask/index.js
+++ web/src/components/GroupTask/index.js
@@ -1,13 +1,21 @@
 import { FiPlus } from 'react-icons/fi'
 import { HiOutlineDotsVertical } from 'react-icons/hi'
-import { TitleContainer, Title, Button, GroupContainer, Text, Group } from './styles';
+import { TitleContainer, Title, Form, Label, Button, GroupContainer, Text, Group } from './styles';
 
 export default function GroupTask() {
     return (
         <>
             <TitleContainer>
-                <Title>Grupo de Tarefas</Title>
-                <Button><FiPlus color="#FFF" size={18} /></Button>
+                <Title>Grupos de Tarefas</Title>
+                <Form action="#">
+                    <Label>
+                        <input type="text" placeholder="Nome do Grupo" />
+                        <Button>
+                            <FiPlus color="#FFF" size={20}/>
+                        </Button>
+                    </Label>
+                    
+                </Form>
             </TitleContainer>
             <GroupContainer>
                 <Text>Todos os grupos</Text>
