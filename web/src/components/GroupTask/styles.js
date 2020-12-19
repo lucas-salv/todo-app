c110c00495d0e68b676b85b4695a3522571921bb
Salv-Dev <47800892+Salv-Dev@users.noreply.github.com>
2020-12-18 23:30:17 -0300

    addGroupForm merged

--- web/src/components/GroupTask/styles.js
+++ web/src/components/GroupTask/styles.js
@@ -2,22 +2,48 @@ import styled from 'styled-components';
 
 export const TitleContainer = styled.div`
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
 `;
 
 export const Title = styled.h4`
     color: ${props => props.theme.textLogoColor};
+    margin-bottom: 5px;
 `;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Label = styled.div`
+    display: flex;
+    background-color: ${props => props.theme.grayBackground};
+    border-radius: 5px;
+
+    input {
+        width: 100%;
+        margin-left: 10px;
+        padding: 10px;
+        color: ${props => props.theme.grayTextColor};
+        background: none;
+        border: none;
+        outline: none;
+
+        &::-webkit-input-placeholder {
+            color: ${props => props.theme.placeholderColor};
+        }
+    }
+`
 
 export const Button = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 5px;
+    padding: 5px 10px;
     border: none;
     background-color: ${props => props.theme.main};
-    border-radius: 5px;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
     cursor: pointer;
 `;
 
@@ -26,7 +52,7 @@ export const GroupContainer = styled.div`
 `;
 
 export const Text = styled.p`
-    font-size: 0.6rem;
+    font-size: 0.7rem;
     color: ${props => props.theme.textLogoColor};
     margin-bottom: 5px;
 `;
@@ -40,6 +66,10 @@ export const Group = styled.div`
     background-color: ${props => props.active ? props.theme.secondary : 'transparent'};
     border-radius: 5px;
     cursor: pointer;
+
+    &:hover {
+        background-color: ${props => props.theme.grayBackground};
+    }
 
     .name {
         color: ${props => props.active ? props.theme.whiteColor : props.theme.grayTextColor};
