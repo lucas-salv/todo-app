8d58ce097b81d87d55b6d49dd238fbcf46c0a345
Salv-Dev <salv.dev.br@gmail.com>
2020-12-19 01:33:43 -0300

    EditTaskForm component created

---
+++ web/src/components/EditTaskForm/styles.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 80%;
+    height: 100vh;
+    padding: 20px;
+    background-color: ${props => props.theme.whiteColor};
+    z-index: 3;
+    transition: transform .5s ease-in-out;
+    transform: translateX(${props => props.open ? 0 : 110}%);
+
+    & ~ .backgroundMenu {
+        z-index: 2;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(0,0,0,0.1);
+        transition: transform .5s ease-in-out;
+        transition: transform .5s ease-in-out;
+        transform: translateX(${props => props.open ? 0 : -110}%);
+    }
+
+    @media (min-width: 600px) {
+        width: 60%;
+    }
+
+    @media (min-width: 900px) {
+        width: 40%;
+    }
+
+    @media (min-width: 1200px) {
+        width: 355px;
+    }
+`
+
+export const Form = styled.form`
+    display:flex;
+    flex-direction: column;
+    width: 100%;
+    transition: width 0.5s ease-in-out;
+`;
+
+export const Label = styled.label`
+    display:flex;
+    align-items: center;
+    padding: 10px;
+    margin-bottom: 10px;
+    background-color: ${props => props.theme.grayBackground};
+    border: 1px solid ${props => props.theme.grayBackground};
+    border-radius: 5px;
+
+    input {
+        width: 100%;
+        margin-left: 10px;
+        color: ${props => props.theme.grayTextColor};
+        background: none;
+        border: none;
+        outline: none;
+
+        &::-webkit-input-placeholder {
+            color: ${props => props.theme.placeholderColor};
+        }
+    }
+
+    textarea {
+        width: 100%;
+        height: 200px;
+        color: ${props => props.theme.grayTextColor};
+        background: none;
+        border: none;
+        outline: none;
+
+        &::-webkit-input-placeholder {
+            color: ${props => props.theme.placeholderColor};
+        }
+    }
+`;
+
+export const Title = styled.h2`
+    font-size: 0.8rem;
+    color: ${props => props.theme.grayTextColor};
+    font-weight: normal;
+    margin: 20px 0 10px 0;
+`
+
+export const Date = styled.p`
+    font-size: 0.8rem;
+    color: ${props => props.theme.grayTextColor};
+    margin-bottom: 10px;
+`
--- web/src/components/Task/index.js
+++ web/src/components/Task/index.js
@@ -1,26 +1,38 @@
+import { useState } from 'react';
 import { FiTrash2 } from 'react-icons/fi';
 import { Container, Tag, Button } from './styles';
 import { Checkbox } from '@trendmicro/react-checkbox';
 import '@trendmicro/react-checkbox/dist/react-checkbox.css';
+import EditTaskForm from './../EditTaskForm';
 
 export default function Task() {
-    return (
-        <Container color="#38A0FF" checked={false} >
-            <Checkbox />
+    const [isEditTaskForm, setEditTaskForm] = useState(false);
 
-            <div className="content-container">
-                <div className="task-container">
-                    <p className="title">Lorem ipsum dolor et...</p>
-                    <div className="tagContainer">
-                        <Tag color="#ed7e2f" />
-                        <Tag color="#ed2f2f" />
-                        <Tag color="#38A0FF" />
+    const EditTaskFormAnimation = () => {
+        setEditTaskForm(!isEditTaskForm);
+    }
+
+    return (
+        <>
+            <Container color="#38A0FF" checked={false} onClick={EditTaskFormAnimation} >
+                <Checkbox />
+
+
+                <div className="content-container">
+                    <div className="task-container">
+                        <p className="title">Lorem ipsum dolor et...</p>
+                        <div className="tagContainer">
+                            <Tag color="#ed7e2f" />
+                            <Tag color="#ed2f2f" />
+                            <Tag color="#38A0FF" />
+                        </div>
                     </div>
+                    <Button>
+                        <FiTrash2 className="trashIcon" />
+                    </Button>
                 </div>
-                <Button>
-                    <FiTrash2 className="trashIcon" />
-                </Button>
-            </div>
-        </Container>
+            </Container>
+            <EditTaskForm open={isEditTaskForm} setOpen={EditTaskFormAnimation}/>
+        </>
     )
 }
