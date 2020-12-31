0154da37ce70e46e4ce5a4955269b13a5760444f
Salv-Dev <salv.dev.br@gmail.com>
2020-12-31 00:16:15 -0300

    createAccountForm validation created

--- web/src/components/FormCreateAccount/styles.js
+++ web/src/components/FormCreateAccount/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Form = styled.form`
+export const Form = styled.div`
     display:flex;
     flex-direction: column;
     width: 100%;
@@ -23,13 +23,28 @@ export const Form = styled.form`
     }
 `;
 
+export const ErrorModal = styled.div`
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 250px;
+    padding: 10px;
+    color: white;
+    background-color: red;
+    border-radius: 5px;
+    transform: ${props => props.status ? 'translateY(0)' : 'translateY(-100px)'};
+    transition: transform .5s;
+`
+
 export const Label = styled.label`
     display:flex;
     align-items: center;
     padding: 10px;
     margin-bottom: 10px;
     background-color: ${props => props.theme.grayBackground};
-    border: 1px solid ${props => props.theme.grayBackground};
+    border: 1px solid ${props => props.status ? 'red' : props.theme.grayBackground};
     border-radius: 5px;
 
     input {
@@ -52,3 +67,23 @@ export const Title = styled.h2`
     font-weight: normal;
     margin-bottom: 10px;
 `
+
+export const Button = styled.button`
+    padding: 10px;
+    margin-bottom: 10px;
+    color: ${props => props.type === 'delete' ? '#ed2f2f' : props.theme.whiteColor};
+    font-weight: bold;
+    outline: none;
+    background-color: ${props => props.type === 'delete' ? props.theme.whiteColor : props.theme.main};
+    border-radius: 5px;
+    border: 1px solid ${props => props.type === 'delete' ? 'transparent' : props.theme.main};
+    cursor: pointer;
+    transition: all .5s ease-in-out;
+
+    &:active {
+        border: 1px solid ${props => props.theme.grayTextColor};
+        color: ${props => props.theme.grayTextColor};
+        background-color: ${props => props.theme.whiteColor};
+    }
+
+`;
--- web/src/utils/errorFunction.js
+++ web/src/utils/errorFunction.js
@@ -2,6 +2,8 @@ export default function errorFunction(statusCode){
     switch (statusCode) {
         case 401 || 500:
             return true;
+        case 400:
+            return true;
         default:
             break;
     }
