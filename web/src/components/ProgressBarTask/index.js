897cfbab0219d48918e2aeda470f8a9eb4a240d6
Salv-Dev <salv.dev.br@gmail.com>
2021-02-09 19:15:14 -0300

    percentage fix

--- web/src/components/ProgressBarTask/index.js
+++ web/src/components/ProgressBarTask/index.js
@@ -10,7 +10,7 @@ export default function ProgressBarTask() {
         const totalTasks = dataActivated ? dataActivated.tasks.length : 0;
         const totalFiltered = dataActivated ? dataActivated.tasks.length > 0 ? dataActivated.tasks.filter(item => item.checked).length : 0 : 0;
         const result = totalTasks === 0 ? 0 : (100 * totalFiltered) / totalTasks;
-        setPercent(result); 
+        setPercent(result.toFixed(0)); 
     }, [dataActivated]);
 
     return (
